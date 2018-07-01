dd2dae21ba070e054276810192069ce54ae6d1f0
albertobebz <alberto.ferioli@icloud.com>
2018-07-01 14:01:13 +0200

    create basic gloabl vars // color palette

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import CommentsList from "./Components/CommentsList";
 import Monitor from "./Components/DisplayMatchInfo";
 import PanelCtrl from "./Components/PanelCtrl";
 
+import { colors } from "./themes";
+import styled, { injectGlobal } from "styled-components";
+
+injectGlobal`
+
+  body{
+    color: ${colors.white}
+  }
+`;
 class App extends Component {
   render() {
     return (
---
+++ src/themes/metrics.js
@@ -0,0 +1,23 @@
+const breakpoints = {
+  // Bootstrap min-width
+  minSmall: 576,
+  minMedium: 768,
+  minLarge: 992,
+  minXL: 1200,
+
+  // Bootstrap max-width
+  maxXSmall: 575.98,
+  maxSmall: 767.98,
+  maxMedium: 991.98,
+  maxLarge: 1199.98
+};
+
+const borders = {
+  // px
+  borderDefault: 6,
+
+  // %
+  borderRadiusRound: 50
+};
+
+export default { breakpoints, borders };
